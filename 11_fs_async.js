0ab29ca842b14218e1ba7d01899a62f4b8a9d64d
Henry Seigbein <hensco95@gmail.com>
2023-03-23 10:58:16 +0100

    new commit

--- 11_fs_async.js
+++ 11_fs_async.js
@@ -1,32 +1,31 @@
 // asynchronous
 
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile, read } = require("fs");
 
 console.log("start");
-
 readFile("./content/first.txt", "utf8", (err, result) => {
   if (err) {
-    console.log(err);
-    return null;
+    console.log(err)
+    return;
   }
+  // set up functionaity here
   const first = result;
   readFile("./content/second.txt", "utf8", (err, result) => {
     if (err) {
       console.log(err);
-      return null;
+      return;
     }
     const second = result;
-    writeFile(
-      "./content/combofile_1_and_2.txt",
-      `Here is the result: ${first} and ${second}`,
-      (err, result) => {
+    writeFile("./content/result-async.txt", `Here is the result : ${first}, ${second}`, { flag: "a" },
+      (err,result) => {
         if (err) {
-          console.log(err);
+          console.log(err)
           return;
         }
-        console.log("done wit this task");
+        console.log("done with this task");
       }
-    );
-  });
+      
+    )
+  })
 });
-console.log("starting next task");
+console.log("starting next file")
